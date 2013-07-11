5aa21e5441af6c462db6962a83c250315d2b6d43
George T. Walters II <walterg2@gmail.com>
2013-07-11 15:46:51 -0400

    Passing second test - Reading a 1

--- groovy/src/main/groovy/com/walterg2/katas/ocrbank/DigitReader.groovy
+++ groovy/src/main/groovy/com/walterg2/katas/ocrbank/DigitReader.groovy
@@ -5,7 +5,7 @@ import com.walterg2.katas.ocrbank.Digit
 class DigitReader {
 
   static def DIGITS = [
-    (Digit.ZERO): '0'
+    (Digit.ZERO): '0', (Digit.ONE): '1'
   ]
   def read(digit) {
     DIGITS[digit]
