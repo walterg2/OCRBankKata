5c47416b64e43aeffdff59600564c8bbba8b6e58
George T. Walters II <walterg2@gmail.com>
2013-07-11 15:37:41 -0400

    Passing first test - Can read a zero

---
+++ groovy/src/main/groovy/com/walterg2/katas/ocrbank/Digit.groovy
@@ -0,0 +1,44 @@
+package com.walterg2.katas.ocrbank
+
+class Digit {
+  static ZERO = ' _ ' +
+                '| |' +
+                '|_|'
+
+  static ONE =  '   ' +
+                '  |' +
+                '  |'
+
+  static TWO =  ' _ ' +
+                ' _|' +
+                '|_ '
+
+  static THREE =  ' _ ' +
+                  ' _|' +
+                  ' _|'
+
+  static FOUR = '   ' +
+                '|_|' +
+                '  |'
+
+  static FIVE = ' _ ' +
+                '|_ ' +
+                ' _|'
+
+  static SIX =  ' _ ' +
+                '|_ ' +
+                '|_|'
+
+  static SEVEN =  ' _ ' +
+                  '  |' +
+                  '  |'
+
+  static EIGHT =  ' _ ' +
+                  '|_|' +
+                  '|_|'
+
+  static NINE = ' _ ' +
+                '|_|' +
+                ' _|'
+
+}
--- groovy/src/main/groovy/com/walterg2/katas/ocrbank/DigitReader.groovy
+++ groovy/src/main/groovy/com/walterg2/katas/ocrbank/DigitReader.groovy
@@ -1,5 +1,13 @@
 package com.walterg2.katas.ocrbank
 
+import com.walterg2.katas.ocrbank.Digit
+
 class DigitReader {
-  
+
+  static def DIGITS = [
+    (Digit.ZERO): '0'
+  ]
+  def read(digit) {
+    DIGITS[digit]
+  }
 }
