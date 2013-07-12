16a5bc6e6a53e1ab3ae92c508cf6d420d4d6995e
George T. Walters II <walterg2@gmail.com>
2013-07-11 20:44:22 -0400

    Building the first Account number parser test

---
+++ groovy/src/test/groovy/com/walterg2/katas/ocrbank/AccountNumberParserSpecification.groovy
@@ -0,0 +1,19 @@
+package com.walterg2.katas.ocrbank
+
+import spock.lang.*
+
+class AccountNumberParserSpecification extends Specification {
+
+  def fileLocation = 'src/test/resources'
+
+  def "Can convert a full account number to it's represented digits"() {
+    given: "I have an account number parser"
+      def parser = new AccountNumberParser()
+    when: "I send it a file with a nine digit account number in it"
+      def file = new File("${fileLocation}/111111111.txt")
+      def result = parser.read(file.readLines())
+    then: "I see the account number represented as digits"
+      result == '111111111'
+  }
+
+}
