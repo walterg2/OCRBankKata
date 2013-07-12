869468cddef04be027a9e25db03e65a24e42bd9c
George T. Walters II <walterg2@gmail.com>
2013-07-11 21:11:33 -0400

    Refactor all the things! Parsing full account numbers now. Also fixed the 999999999 file to use the right representation of 9s. Doing katas while taking care of kids is distracting.

--- groovy/src/test/groovy/com/walterg2/katas/ocrbank/AccountNumberParserSpecification.groovy
+++ groovy/src/test/groovy/com/walterg2/katas/ocrbank/AccountNumberParserSpecification.groovy
@@ -6,24 +6,24 @@ class AccountNumberParserSpecification extends Specification {
 
   def fileLocation = 'src/test/resources'
 
+  @Unroll("Converting a file with account number #accountNumber")
   def "Can convert a full account number to it's represented digits"() {
     given: "I have an account number parser"
       def parser = new AccountNumberParser()
     when: "I send it a file with a nine digit account number in it"
-      def file = new File("${fileLocation}/111111111.txt")
+      def file = new File("${fileLocation}/${accountNumber}.txt")
       def result = parser.read(file.readLines())
     then: "I see the account number represented as digits"
-      result == '111111111'
-  }
-
-  def "Can convert another account number to it's represented digits"() {
-    given: "I have an account number parser"
-      def parser = new AccountNumberParser()
-    when: "I send it a file with a nine digit account number in it"
-      def file = new File("${fileLocation}/123456789.txt")
-      def result = parser.read(file.readLines())
-    then: "I see the account number represented as digits"
-      result == '123456789'
+      println "Result: ${result} == AccountNumber: ${accountNumber}"
+      result == accountNumber
+    where:
+      accountNumber << [
+        '111111111',
+        '123456789',
+        '127538461',
+        '732589640',
+        '999999999'
+      ]
   }
 
 }
