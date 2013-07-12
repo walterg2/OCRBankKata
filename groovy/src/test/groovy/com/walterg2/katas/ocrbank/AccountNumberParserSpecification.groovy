57d723184ce8f6c2310f02b35ac39dce5944118d
George T. Walters II <walterg2@gmail.com>
2013-07-11 21:00:39 -0400

    And it works! BOOYAH

--- groovy/src/test/groovy/com/walterg2/katas/ocrbank/AccountNumberParserSpecification.groovy
+++ groovy/src/test/groovy/com/walterg2/katas/ocrbank/AccountNumberParserSpecification.groovy
@@ -16,4 +16,14 @@ class AccountNumberParserSpecification extends Specification {
       result == '111111111'
   }
 
+  def "Can convert another account number to it's represented digits"() {
+    given: "I have an account number parser"
+      def parser = new AccountNumberParser()
+    when: "I send it a file with a nine digit account number in it"
+      def file = new File("${fileLocation}/123456789.txt")
+      def result = parser.read(file.readLines())
+    then: "I see the account number represented as digits"
+      result == '123456789'
+  }
+
 }
